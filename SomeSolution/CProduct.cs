6f2547fcf777b541a03a613a9709f0ed58dc681c
Dumitru <magas.dm99@gmail.com>
2024-03-01 17:19:27 +0100

    Init commit

---
+++ SomeSolution/CProduct.cs
@@ -0,0 +1,24 @@
+﻿namespace SomeSolution;
+
+public class CProduct
+{
+    public string Name { get; private set; }
+    public int Price { get; private set; }
+
+    public CProduct(string name, int price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public void ChangeName(string name)
+    {
+        this.Name = name;
+    }
+
+    public void ChangePrice(int price)
+    {
+        this.Price = price;
+    }
+    
+}
